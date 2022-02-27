e1e20a27f93c1d3b19fc34ea378cb6fda13113d3
yangshangdong <947899976@qq.com>
2022-02-27 16:38:46 -0500

    Create buzzer_funtion_1.ino

---
+++ arduino_project/steven/buzzer_funtion_1.ino
@@ -0,0 +1,23 @@
+
+int buzzer = 12;//the pin of the active buzzer
+void setup()
+{
+  pinMode(buzzer,OUTPUT);//initialize the buzzer pin as an output
+  buzzer_n_times(5); //n for the buzzer times
+
+}
+void loop()
+{
+} 
+
+void buzzer_n_times(int n) // n for multiple times
+{
+  unsigned char i; 
+  for(i=0;i<n;i++)
+  {
+    tone(buzzer,200);
+    delay(1000);
+    noTone(buzzer);
+    delay(500);
+  }
+}
